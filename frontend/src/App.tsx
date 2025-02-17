20ad2f96898d34328d74c81c1b3c48ce2d3f9b9b
LJulien27 <ljuli048@uOttawa.ca>
2025-02-16 19:26:06 -0500

    At first the React project was in JS, had to make a new one in TS for the components to mix properly, should be working better now, frontend-old is simply the old project, I kept it for now incase there was more stuff missing from the old one and didnt want to accidentally delete anything useful

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import QuizCreate from './components/QuizCreate/QuizCreate';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <QuizCreate />
+    </div>
+  );
+};
+
+export default App;
+
