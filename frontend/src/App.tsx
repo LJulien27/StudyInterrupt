3eceb54bcf5d1dacb644c6c0f86c338822e8ca69
Luca Balan <113710003+lbala022@users.noreply.github.com>
2025-03-20 19:11:01 -0400

    first commit to new branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { Container, Navbar, Nav } from 'react-bootstrap';
 import QuizCreate from './components/QuizCreate/QuizCreate';
 import UserForm from './components/UserForm/UserForm';
 import Quiz from './components/Quiz/Quiz';
+import MySessions from './components/MySessions/MySessions';
+import MyQuizContent from './components/MyQuizContent/MyQuizContent';
 
 const App: React.FC = () => {
   return (
@@ -18,6 +20,8 @@ const App: React.FC = () => {
               <Nav.Link as={Link} to="/quiz">Quizzes</Nav.Link>
               <Nav.Link as={Link} to="/create-quiz">Create Your Own Quiz</Nav.Link>
               <Nav.Link as={Link} to="/user-form">User Parameters</Nav.Link>
+              <Nav.Link as={Link} to="/my-sessions">My Sessions</Nav.Link>
+              <Nav.Link as={Link} to="/my-quiz-content">My Quiz Content</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -28,6 +32,8 @@ const App: React.FC = () => {
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/create-quiz" element={<QuizCreate />} />
           <Route path="/user-form" element={<UserForm />} />
+          <Route path="/my-sessions" element={<MySessions />} />
+          <Route path="/my-quiz-content" element={<MyQuizContent />} />
         </Routes>
       </Container>
     </Router>
