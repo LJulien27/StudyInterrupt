df86bad397e037cb21603e40a6c8a67955b3d745
oguindon <91165057+oguindon@users.noreply.github.com>
2025-03-30 21:33:40 -0400

    Comments for popup.js

--- oauth2.0/popup.js
+++ oauth2.0/popup.js
@@ -1,6 +1,10 @@
+// Listener for the open web app button. Opens index.html as a web page.
 document.addEventListener("DOMContentLoaded", () => {
     const openWebAppButton = document.getElementById("openWebApp");
-    if (openWebAppButton) {
+    
+    // verification
+    if (openWebAppButton) { 
+        // create web page
         openWebAppButton.addEventListener("click", () => {
             chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
         });
@@ -9,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+/* Commented out listener for the login button */
 // document.getElementById("login-btn").addEventListener("click", function() {
 //     chrome.runtime.sendMessage({ action: "login" }, function(response) {
 //         if (response.success) {
